a83247cc55edb4a2d7fe7d182dec9d6204d60266
Jeevan <justjeevan07@gmail.com>
2026-01-22 08:30:18 +0530

    refactor: Simplify main.tsx entry point

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,40 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import { LoginScreen } from './components/Auth/LoginScreen';
-import { DashboardScreen } from './components/Dashboard/DashboardScreen';
-import { MapEditorScreen } from './components/Editor/MapEditorScreen';
-import { ProtectedRoute } from './components/Auth/ProtectedRoute';
 import './index.css';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginScreen />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardScreen />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/editor"
-          element={
-            <ProtectedRoute requireVillage>
-              <MapEditorScreen />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-      <Toaster position="top-center" />
-    </BrowserRouter>
-  );
-}
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
